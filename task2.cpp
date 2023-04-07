9608ccb9c3a721308e89666caa4ef40ed042c72f
ArthurLCW <arthurlicw@gmail.com>
2023-04-07 09:02:37 +0800

    please use REFERENCE/POINTER as function paramenter

--- task2.cpp
+++ task2.cpp
@@ -1,12 +1,9 @@
 #include <map>
 #include <iostream>
 using namespace std;
-int countMapElements(map<string, string> place_map){
-    return place_map.size();
-}
 
-/** Need further extension and enhancement! */
-string getGCC(map<string, string> place_map, map<string, int> gcc_count_map, string place_name){
+/** TODO: Need further extension and enhancement! */
+string getGCC(map<string, string> &place_map, map<string, int> &gcc_count_map, string place_name){
     // cout<<"getGCC0 place name: "<<place_name<<endl;
     if (place_map.count(place_name)>0){ // the place name exists directly inside the place name map.
         // cout<<"getGCC1 place name: "<<place_name<<endl;
@@ -14,9 +11,11 @@ string getGCC(map<string, string> place_map, map<string, int> gcc_count_map, str
         if (gcc_count_map.count(gcc_name)>0) return place_map[place_name];
     }
     return "";
+
+//     return "1gsyd";
 }
 
-void task2(map<string, int> &gcc_count_map, map<string, string> place_map, string place_name){
+void task2(map<string, int> &gcc_count_map, map<string, string> &place_map, string place_name){
     // firstly, get real name, delete comma
     size_t pos_comma = place_name.find(",");
     if (pos_comma!=string::npos){ // there is such a comma
@@ -31,7 +30,7 @@ void task2(map<string, int> &gcc_count_map, map<string, string> place_map, strin
     }
 }
 
-void testTask2(map<string, int> gcc_count_map){
+void testTask2(map<string, int> &gcc_count_map){
     std::map<std::string, int>::iterator it = gcc_count_map.begin();
     // Iterate through the map and print the elements
     while (it != gcc_count_map.end()){
